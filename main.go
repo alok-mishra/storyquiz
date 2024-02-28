64d334f21edf49ef6db906cf5c1ea7dfe5db76d9
Alok Mishra <alok@alokmishra.com>
2024-02-28 17:26:40 -0500

    read xml file directly from docx file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
@@ -9,7 +10,7 @@ import (
 	"regexp"
 )
 
-const filePath = "tables.xml"
+const docxFile = "tables.docx"
 
 type Body struct {
 	Tables []Table `xml:"tbl"`
@@ -28,27 +29,61 @@ type Cell struct {
 }
 
 func main() {
-	// Open the file
-	file, err := os.Open(filePath)
+	// Open the .docx file
+	docx, err := os.Open(docxFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer docx.Close()
+
+	// Get information about the .docx file
+	docxInfo, err := docx.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read the document.xml file from the .docx file
+	r, err := zip.NewReader(docx, docxInfo.Size())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var xmlFile *zip.File
+	for _, f := range r.File {
+		if f.Name == "word/document.xml" {
+			xmlFile = f
+			break
+		}
+	}
+
+	if xmlFile == nil {
+		log.Fatal("word/document.xml not found in the .docx file")
+	}
+
+	file, err := xmlFile.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	// Read the file contents
-	fileInfo, err := file.Stat()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fileSize := fileInfo.Size()
-	xmlData := make([]byte, fileSize)
-	_, err = file.Read(xmlData)
-	if err != nil {
-		log.Fatal(err)
+	// Read the content of the document.xml file into a byte slice
+	var xmlData []byte
+	buffer := make([]byte, 1024)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil && err.Error() != "EOF" {
+			log.Fatal(err)
+		}
+		if n == 0 {
+			break
+		}
+		xmlData = append(xmlData, buffer[:n]...)
 	}
 
+	// fmt.Println(string(xmlData))
+
 	cleanedXML := cleanXML(string(xmlData))
-	// fmt.Println(cleanedXML)
+	fmt.Println(cleanedXML)
 
 	var body Body
 	if err := xml.Unmarshal([]byte(cleanedXML), &body); err != nil {
@@ -64,14 +99,11 @@ func main() {
 }
 
 func cleanXML(content string) string {
-
+	// fmt.Println(content)
 	body := regexp.MustCompile(`(?s)<w:body>(.*?)<\/w:body>`).FindString(content)
 	// fmt.Println(body)
 	re := regexp.MustCompile(`<(/?)w:([^/>\s]+)[^/>]*(/?)>`)
-
 	cleanedBody := re.ReplaceAllString(body, "<$1$2$3>")
-
-	fmt.Println(cleanedBody)
-
+	// fmt.Println(cleanedBody)
 	return cleanedBody
 }
