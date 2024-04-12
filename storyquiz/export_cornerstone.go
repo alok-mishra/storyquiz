ffb8f211c2580b7bbf3d664cd0de2ccab24c8352
Alok Mishra <alok@alokmishra.com>
2024-04-12 18:06:39 -0400

    update header and dynamically output answers and count

--- storyquiz/export_cornerstone.go
+++ storyquiz/export_cornerstone.go
@@ -23,7 +23,7 @@ func exportCornerstone(questions []Question) {
 
 	f.SetActiveSheet(index)
 
-	const header = "Question ID,Question Text,Question Type,Correct Answer,Category Id,Default Language,Active,Randomize Answer Choices,Answer Explanation,# of Answer Choices,All of the Above,None of the Above,Answer 1,Answer 2,Answer 3,Answer 4,Answer 5,Answer 6,Answer 7,Answer 8,Answer 9,Answer 10,Image Filename,Answer Coordinates,Apply partial scoring,Author"
+	const header = "Question Reference Number,Question Text,Question Type,Correct Answer,Category Id,Default Language,Active,Randomize Answer Choices,Answer Explanation,# of Answer Choices,All of the Above,None of the Above,Answer 1,Answer 2,Answer 3,Answer 4,Answer 5,Answer 6,Answer 7,Answer 8,Answer 9,Answer 10,Image Filename,Answer Coordinates,Apply partial scoring,Author"
 
 	// loop through the header and write to the sheet
 	for i, v := range strings.Split(header, ",") {
@@ -32,7 +32,7 @@ func exportCornerstone(questions []Question) {
 
 	// for each question, write to the sheet
 	for i, question := range questions {
-		f.SetCellValue(sheetName, fmt.Sprintf("A%d", i+2), "")
+		f.SetCellValue(sheetName, fmt.Sprintf("A%d", i+2), i+1)
 		f.SetCellValue(sheetName, fmt.Sprintf("B%d", i+2), question.QuestionText)
 		f.SetCellValue(sheetName, fmt.Sprintf("C%d", i+2), "Multiple Choice / Single Answer")
 		f.SetCellValue(sheetName, fmt.Sprintf("D%d", i+2), 1)
@@ -41,13 +41,12 @@ func exportCornerstone(questions []Question) {
 		f.SetCellValue(sheetName, fmt.Sprintf("G%d", i+2), 1)
 		f.SetCellValue(sheetName, fmt.Sprintf("H%d", i+2), 1)
 		f.SetCellValue(sheetName, fmt.Sprintf("I%d", i+2), "")
-		f.SetCellValue(sheetName, fmt.Sprintf("J%d", i+2), 4)
+		f.SetCellValue(sheetName, fmt.Sprintf("J%d", i+2), len(question.Options))
 		f.SetCellValue(sheetName, fmt.Sprintf("K%d", i+2), "")
 		f.SetCellValue(sheetName, fmt.Sprintf("L%d", i+2), "")
-		f.SetCellValue(sheetName, fmt.Sprintf("M%d", i+2), question.Options[0].Text)
-		f.SetCellValue(sheetName, fmt.Sprintf("N%d", i+2), question.Options[1].Text)
-		f.SetCellValue(sheetName, fmt.Sprintf("O%d", i+2), question.Options[2].Text)
-		f.SetCellValue(sheetName, fmt.Sprintf("P%d", i+2), question.Options[3].Text)
+		for j, option := range question.Options {
+			f.SetCellValue(sheetName, fmt.Sprintf("%c%d", 'M'+j, i+2), option.Text)
+		}
 	}
 
 	// delete the default sheet
